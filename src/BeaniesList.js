ed8f04746b990b0f3c26f9a1eca35c7aa6f6a7da
Philippe Ngom <philngom@gmail.com>
2022-02-07 21:30:52 -0800

    added path to Route in App.js, beanie baby page displaying

--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -4,5 +4,10 @@ export default function BeaniesList({ beanieBabies }) {
   return (
     <div className='beanies'>
       {/* render out the beanie babies as a list */}
+      {
+        beanieBabies.map((beanieBaby, i) =>
+          <BeanieBaby key={beanieBaby + i} beanieBaby={beanieBaby}/>
+        )
+      }
     </div>);
 }
